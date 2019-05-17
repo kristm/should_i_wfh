e00a3c5716e83c6a57e7e78a75f21f450b7bf801
Krist Menina <krist@quipper.com>
2019-05-18 01:55:06 +0800

    remove duplicate names

--- tensorflowproj/webapp/urls.py
+++ tensorflowproj/webapp/urls.py
@@ -1,4 +1,4 @@
-"""tensorflowproj URL Configuration
+"""webapp URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
